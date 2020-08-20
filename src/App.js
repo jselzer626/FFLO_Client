f45c80e13462380eb34312f7616e6d8088cbf4f7
Jon Selzer <jselzer626@gmail.com>
2020-08-20 15:59:53 -0400

    started to add player filter

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import React, { useState } from 'react'
 function App() {
 
     const [fullPlayerList, setFullPlayerList] = useState([])
-    const [filteredList, setFilteredList] = useState([])
+    const [filteredPlayerList, setFilteredPlayerList] = useState([])
     const [input, setInput] = useState({playerSearch: ''})
     const [loading, setLoading] = useState({initialLoading: true, loading: false})
     //const [step, setStep] = ({playerList: 100})
+
+    const handleInputChange = e => {
+        let fullList = fullPlayerList
+        let newPlayerList = fullList.filter((player) => {
+            player.displayName.includes("th")
+        })
+        setFilteredPlayerList(newPlayerList)
+    }
+    
+    const renderInputForm = () => {
+        return (
+            <div className = "ui form">
+                <input className = "ui input"
+                    type="text"
+                    onChange={handleInputChange}
+                >
+                </input>
+            </div>
+        )
+    }
 
     const loadPlayerList = async () => {
         //e.preventDefault()
@@ -14,7 +34,6 @@ function App() {
         try {
             let playerList = await fetch('http://127.0.0.1:8000/players/loadInitial')
             let playerListJson = await playerList.json()
-            console.log(playerListJson)
             setFullPlayerList(playerListJson)
         } catch(e) {
             console.warn(e)
@@ -46,23 +65,39 @@ function App() {
             loadPlayerList()
             setLoading({...loading,initialLoading:false})
         }
-        return (
-            //need to make this a scrollable box with a max height
-            //check semantic ui containers
-            <div>
-                <div className="ui items">
-                    {fullPlayerList.map((player, index) => {
-                        if (index < 100)
-                            return renderPlayerCard(player)
-                    })}
+        if (!filteredPlayerList) {
+            return (
+                //need to make this a scrollable box with a max height
+                //check semantic ui containers
+                <div>
+                    <div className="ui items">
+                        {fullPlayerList.map((player, index) => {
+                            if (index < 100)
+                                return renderPlayerCard(player)
+                        })}
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
+        else {
+            return (
+                //need to make this a scrollable box with a max height
+                //check semantic ui containers
+                <div>
+                    <div className="ui items">
+                        {filteredPlayerList.map((player, index) => {
+                            return renderPlayerCard(player)
+                        })}
+                    </div>
+                </div>
+            )
+        }
     }
 
     return (
         <div className="App">
             <div className="ui text container">
+                {renderInputForm()}
                 {renderPlayerList()}
             </div>
         </div>
