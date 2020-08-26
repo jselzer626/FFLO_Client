6682f9d9971ba11ffe74c3e5d7d4d93e0efae71c
Jon Selzer <jselzer626@gmail.com>
2020-08-26 16:39:16 -0400

    Updates

---
+++ precache-manifest.272edbde20afd31566170a6998e8163f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5708b3827fc7641f318cbbaa31deae1e",
+    "url": "/FFLO_Client/index.html"
+  },
+  {
+    "revision": "235136404b936b0c3637",
+    "url": "/FFLO_Client/static/css/main.ea78d578.chunk.css"
+  },
+  {
+    "revision": "214f3d8b6cf5273ce4f3",
+    "url": "/FFLO_Client/static/js/2.a2fceec2.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/FFLO_Client/static/js/2.a2fceec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235136404b936b0c3637",
+    "url": "/FFLO_Client/static/js/main.c1cf194e.chunk.js"
+  },
+  {
+    "revision": "5339797cd7071c153886",
+    "url": "/FFLO_Client/static/js/runtime-main.5cf9b6e9.js"
+  },
+  {
+    "revision": "79afee203353e2c93c045d51a98f47f5",
+    "url": "/FFLO_Client/static/media/Loading_Spinner.79afee20.gif"
+  }
+]);
